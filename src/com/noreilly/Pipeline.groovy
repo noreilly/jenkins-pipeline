ecc933b4361be092a6112463ea56d04df16c8946
Niall O'Reilly <noreill@gmail.com>
2018-04-24 19:09:45 +0100

    Helm wait arg added

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -96,7 +96,7 @@ def helmDeployRaw(Map args) {
         println "Running deployment"
 
         // reimplement --wait once it works reliable
-        sh "helm upgrade --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag},replicas=${args.replicas},cpu=${args.cpu},memory=${args.memory},ingress.hostname=${args.hostname} --namespace=${namespace}"
+        sh "helm upgrade --wait --install ${args.name} ${args.chart_dir} --set image.tag=${args.version_tag},replicas=${args.replicas},cpu=${args.cpu},memory=${args.memory},ingress.hostname=${args.hostname} --namespace=${namespace}"
 
         // sleeping until --wait works reliably
         sleep(20)
