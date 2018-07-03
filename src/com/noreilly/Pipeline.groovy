1d02926fc3136191a961b2978f73fd79ebe0798a
noreilly <noreill@gmail.com>
2018-07-03 16:29:58 +0100

    Update Pipeline.groovy
    typo corrected

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -12,8 +12,8 @@ def baseTemplate(body){
     volumes: [
         hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
     ]){
--        body()
--    }
+        body()
+    }
 }
 
 def kubectlTestConnectivity() {
