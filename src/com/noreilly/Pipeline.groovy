7b553f4f2478b25fcf7a197b03fe45576bed83bc
noreilly <noreill@gmail.com>
2018-07-08 18:20:50 +0100

    Update Pipeline.groovy

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -66,8 +66,7 @@ def switchKubeContext(){
 		     echo '$CLOUD_CREDENTIALS' > /tmp/creds.json;
 		     cat /tmp/creds.json;
 		     gcloud auth activate-service-account --key-file /tmp/creds.json;
-		     export GOOGLE_APPLICATION_CREDENTIALS=/tmp/creds.json;
-		     export CLOUDSDK_CONTAINER_USE_CLIENT_CERTIFICATE=True
+		     export CLOUDSDK_CORE_PROJECT=portdynamics
 		     gcloud container clusters get-credentials $CLOUD_TEST_CLUSTER_NAME  --zone $CLOUD_TEST_CLUSTER_ZONE
 		     kubectl get pods
 	     """
