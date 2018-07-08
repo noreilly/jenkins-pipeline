0d52a99cd16958a7d80a00ad48402835c5d9f87b
noreilly <noreill@gmail.com>
2018-07-08 18:41:34 +0100

    Update Pipeline.groovy

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -67,7 +67,6 @@ def switchKubeContext(){
 		     cat /tmp/creds.json;
 		     gcloud auth activate-service-account --key-file /tmp/creds.json;
 		     export CLOUDSDK_CORE_PROJECT=portdynamics
-		     tail -f /dev/null
 		     gcloud container clusters get-credentials $CLOUD_TEST_CLUSTER_NAME  --zone $CLOUD_TEST_CLUSTER_ZONE
 		     kubectl get pods
 	     """
