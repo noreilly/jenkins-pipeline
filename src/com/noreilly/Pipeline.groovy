2623fcffcd0e4883219eedea14d2c1728f9b2b6a
Ian Duffy <1243435+imduffy15@users.noreply.github.com>
2018-08-14 14:54:49 +0100

    Update Pipeline.groovy

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/groovy
 package com.noreilly;
 
+import org.yaml.snakeyaml.Yaml
+
 def baseTemplate(body) {
     podTemplate(label: 'jenkins-pipeline', idleMinutes: 1440, containers: [
         containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.19-1', args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins', ttyEnabled: true),
