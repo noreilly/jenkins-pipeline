762a1ee377820a662e9b25a347385e33b9483891
Ian Duffy <1243435+imduffy15@users.noreply.github.com>
2018-10-18 14:48:24 +0100

    Update Pipeline.groovy

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -4,7 +4,7 @@ package com.noreilly;
 def baseTemplate(body) {
     podTemplate(label: 'jenkins-pipeline', idleMinutes: 1440, containers: [
         containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.26-1', args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins', ttyEnabled: true),
-        containerTemplate(name: 'mvn', image: 'maven:3-jdk-10', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'mvn', image: 'imduffy15/docker-java:0.0.1', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'golang', image: 'imduffy15/docker-golang:0.0.2', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'node', image: 'imduffy15/docker-frontend:0.0.1', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'docker', image: 'imduffy15/docker-gcloud:0.0.1', command: 'cat', ttyEnabled: true),
