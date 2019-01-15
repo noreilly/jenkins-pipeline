2be02997553e711d3b66a57d90ba2bd53ebde86d
Ian Duffy <1243435+imduffy15@users.noreply.github.com>
2019-01-15 12:03:18 +0000

    Update Pipeline.groovy

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -8,12 +8,12 @@ def baseTemplate(body) {
 
     ])
     podTemplate(label: 'jenkins-pipeline', idleMinutes: 1440, containers: [
-        containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.26-1', args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins', ttyEnabled: true),
-        containerTemplate(name: 'mvn', image: 'imduffy15/docker-java:0.0.2', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'golang', image: 'imduffy15/docker-golang:0.0.2', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'node', image: 'imduffy15/docker-frontend:0.0.1', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'docker', image: 'imduffy15/docker-gcloud:0.0.1', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'helm', image: 'imduffy15/helm-kubectl:3.0.0', command: 'cat', ttyEnabled: true)
+        containerTemplate(name: 'jnlp', image: 'imduffy15/jnlp-slave:3.27-1-alpine-2', args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins', ttyEnabled: true),
+        containerTemplate(name: 'mvn', image: 'imduffy15/docker-java:0.0.3', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'golang', image: 'imduffy15/docker-golang:0.0.3', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'node', image: 'imduffy15/docker-frontend:0.0.2', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'docker', image: 'imduffy15/docker-gcloud:0.0.3.', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'helm', image: 'imduffy15/helm-kubectl:3.0.1', command: 'cat', ttyEnabled: true)
     ],
         volumes: [
             hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
