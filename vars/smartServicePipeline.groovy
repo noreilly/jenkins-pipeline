dc6e2fb85bd5fa0465d13f16dbac6075eb449b74
Enrico Colasante <enrico.colasante@shipyardtech.com>
2019-03-20 15:46:50 +0100

    Add sync ui components function

--- vars/smartServicePipeline.groovy
+++ vars/smartServicePipeline.groovy
@@ -95,7 +95,7 @@ def call(validateAndDeployUi = false) {
                 if(validateAndDeployUi) {
                     stage('Trigger ui builder') {
                         container('mvn') {
-                            build job: "/organization_scan/job//shipyard/factory/vuegg-fork", wait: false
+                            build job: "/organization_scan/job/shipyard/factory/vuegg-fork", wait: false
                         }
                     }
                 }
