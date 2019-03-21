bdeb826e490da8a18363f6e1e3305c4c09a13932
Enrico Colasante <enrico.colasante@shipyardtech.com>
2019-03-21 18:40:00 +0100

    Fix sync components function

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -174,7 +174,7 @@ def syncComponents() {
 #!/bin/bash
 cd client/src/assets/smartServices
 gsutil -m cp -R gs://sy-ui-components/smart-services/*/stable/* .
-cat *-configuration.json | jq .dependencies[].command > ../../../dependencies.txt
+cat *-configuration.json | jq -r .dependencies[].command > ../../../dependencies.txt
 cat ../../../dependencies.txt
 '''
 }
