2b92faf7412a6c879de02ec640966921623bdf25
Enrico Colasante <enrico.colasante@shipyardtech.com>
2019-03-21 16:46:50 +0100

    Fix sync components function

--- src/com/noreilly/Pipeline.groovy
+++ src/com/noreilly/Pipeline.groovy
@@ -196,11 +196,11 @@ def publishUi() {
         sh '''
 #!/bin/bash
 gsutil cp ui/widgets/${SHORT_NAME_CAPITAL_LETTER}.js gs://sy-ui-components/smart-services/${NAME}/${IMAGE_TAG}/
-gsutil cp ui/widgets/${SHORT_NAME}-dependencies.txt gs://sy-ui-components/smart-services/${NAME}/${IMAGE_TAG}/
+gsutil cp ui/widgets/${SHORT_NAME}-configuration.json gs://sy-ui-components/smart-services/${NAME}/${IMAGE_TAG}/
 gsutil -m cp -r ui/widgets/${SHORT_NAME} gs://sy-ui-components/smart-services/${NAME}/${IMAGE_TAG}/
 
 gsutil cp ui/widgets/${SHORT_NAME_CAPITAL_LETTER}.js gs://sy-ui-components/smart-services/${NAME}/stable/
-gsutil cp ui/widgets/${SHORT_NAME}-dependencies.txt gs://sy-ui-components/smart-services/${NAME}/stable/
+gsutil cp ui/widgets/${SHORT_NAME}-configuration.json gs://sy-ui-components/smart-services/${NAME}/stable/
 gsutil -m cp -r ui/widgets/${SHORT_NAME} gs://sy-ui-components/smart-services/${NAME}/stable/
 
 '''
